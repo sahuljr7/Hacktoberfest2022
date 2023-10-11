2a85f597c809e1f7ee0c46729ab5b287c2cdafdc
NotSoDev <108209653+notsodevv@users.noreply.github.com>
2023-10-12 02:20:23 +0530

    Create pattern4.java

---
+++ Java Problem/pattern4.java
@@ -0,0 +1,27 @@
+public class DiamondStarPattern {
+    public static void main(String[] args) {
+        int n = 5; // Change this value to adjust the size of the diamond
+
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
